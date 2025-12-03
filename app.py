87c88c50f72d60fb33cbebdce426e604507c11b2
astha-gami <asthagami2707@gmail.com>
2025-12-03 18:06:04 +0530

    added pdf preview and seperate pages for login, register, dashboard, landing

--- app.py
+++ app.py
@@ -185,8 +185,21 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 # FRONTEND HOME PAGE
 # ----------------------
 @app.route("/")
-def index():
-    return render_template("index.html")
+def landing():
+    return render_template("landing.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/login")
+def login_page():
+    return render_template("login.html")
+
+@app.route("/register") 
+def register_page():
+    return render_template("register.html")
+
 
 
 # ----------------------
